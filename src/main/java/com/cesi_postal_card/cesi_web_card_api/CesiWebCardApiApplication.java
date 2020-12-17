ecd9b2e9770a19bbc170a94b65053888b5e1e0bc
NawakChevelu <garfield-12@hotmail.fr>
2020-12-17 11:55:42 +0100

    ajout structure Spring

---
+++ src/main/java/com/cesi_postal_card/cesi_web_card_api/CesiWebCardApiApplication.java
@@ -0,0 +1,13 @@
+package com.cesi_postal_card.cesi_web_card_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesiWebCardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CesiWebCardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesi_postal_card/cesi_web_card_api/CesiWebCardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesi_postal_card.cesi_web_card_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CesiWebCardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
